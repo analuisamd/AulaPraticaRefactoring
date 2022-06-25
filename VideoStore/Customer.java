b2ecdda9a29a3c40b22181974f15e7adaa12f6fd
ana luisa <55565972+analuisamd@users.noreply.github.com>
2022-06-25 17:02:25 -0300

    Commit 2

--- VideoStore/Customer.java
+++ VideoStore/Customer.java
@@ -5,6 +5,31 @@ public class Customer {
    private String _name;
    private Vector _rentals = new Vector();
 
+
+   private double amountFor(Rental each) {
+      //Adicionar o trecho de código extraído.
+      double thisAmount = 0;
+      //determine amounts for each line
+        
+
+      switch (each.getMovie().getPriceCode()) {
+         case Movie.REGULAR:
+            thisAmount += 2;
+            if (each.getDaysRented() > 2)
+               thisAmount += (each.getDaysRented() - 2) * 1.5;
+            break;
+         case Movie.NEW_RELEASE:
+            thisAmount += each.getDaysRented() * 3;
+            break;
+         case Movie.CHILDRENS:
+            thisAmount += 1.5;
+            if (each.getDaysRented() > 3)
+               thisAmount += (each.getDaysRented() - 3) * 1.5;
+             break;
+      }
+   }
+
+
    public Customer (String name){
       _name = name;
    }
@@ -25,23 +50,6 @@ public class Customer {
      while (rentals.hasMoreElements()) {
         double thisAmount = 0;
         Rental each = (Rental) rentals.nextElement();
-
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
 
         // add frequent renter points
         frequentRenterPoints ++;
