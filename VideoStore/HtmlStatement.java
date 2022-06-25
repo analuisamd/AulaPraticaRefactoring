5d0d3f2f9feaa7162dfff54884c6d889a05b2d18
ana luisa <55565972+analuisamd@users.noreply.github.com>
2022-06-25 18:36:05 -0300

    Commit 18

--- VideoStore/HtmlStatement.java
+++ VideoStore/HtmlStatement.java
@@ -1,37 +1,18 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
-   public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = getBeginningText(aCustomer);
+  
+    protected String getBeginningText(Customer aCustomer) {
+        return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
+  }
 
-
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += getContentText(each);
-      }
-      //add footer lines
-      result += getEndText(aCustomer);
-      return result;
+    protected String getContentText(Rental aRental) {
+        return aRental.getMovie().getTitle()+ ": " +
+             String.valueOf(aRental.getCharge()) + "<BR>\n";
    }
 
-   private String getBeginningText(Customer aCustomer) {
-    return "<H1>Rentals for <EM>" + aCustomer.getName() +
-      "</EM></H1><P>\n";
-  }
-
-  private String getContentText(Rental aRental) {
-    return aRental.getMovie().getTitle()+ ": " +
-         String.valueOf(aRental.getCharge()) + "<BR>\n";
-  }
-
-  private String getEndText(Customer aCustomer) {
-    String result = "<P>You owe <EM>" +
-    String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-    result += "On this rental you earned <EM>" + 
-    String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-    "</EM> frequent renter points<P>";
-    return result;
+  protected String getEndText(Customer aCustomer) {
+        String result = "<P>You owe <EM>" +
+        String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" + String.valueOf(aCustomer.getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+        return result;
   }
 }
