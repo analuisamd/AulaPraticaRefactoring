8b04bdded8e57089f4244d54ddb96e4e4e508abf
ana luisa <55565972+analuisamd@users.noreply.github.com>
2022-06-25 17:46:39 -0300

    Commit 9

--- VideoStore/Rental.java
+++ VideoStore/Rental.java
@@ -24,25 +24,7 @@ public class Rental {
    }
 
    public double getCharge() {
-      //Adicionar o trecho de código extraído.
-      double thisAmount = 0;
-      //determine amounts for each line
-
-      switch (this.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (each.getDaysRented() > 2)
-               thisAmount += (this.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += this.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (getDaysRented() > 3)
-               thisAmount += (this.getDaysRented() - 3) * 1.5;
-             break;
-      }
+      return _movie.getCharge(_daysRented);
    }
 
 
